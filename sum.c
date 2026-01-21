cc9634ca8fb17c5f271a5b67bcf60958e920743e
Abu Abdullah Muhammad Asad <a.a.m.asad2004@gmail.com>
2026-01-22 02:25:14 +0600

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float x,y,sum;
+    printf("Enter two number : ");
+    scanf("%f %f",&x,&y);
+
+    sum=x+y;
+    printf("Sum of two number = %f ", sum);
+    return 0;
+}
