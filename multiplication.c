28a72a76af772d82919a4bc26238baf74822f792
Abu Abdullah Muhammad Asad <a.a.m.asad2004@gmail.com>
2026-01-25 00:24:47 +0600

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float x,y,mul;
+    printf("Enter 1st number : ");
+    scanf("%f", &x);
+    printf("Enter 2nd number : ");
+    scanf("%f", &y);
+
+    mul=x*y;
+    printf("The multiplication= %f", mul);
+
+    return 0;
+
+}
