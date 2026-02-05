1addbe27cd375bfc10ea0d5f9f1b6ce0c3ab006b
Abu Abdullah Muhammad Asad <a.a.m.asad2004@gmail.com>
2026-02-05 22:38:00 +0600

    Add files via upload

---
+++ min & max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("Maximum = %d\n", a);
+        printf("Minimum = %d\n", b);
+    } else if (b > a) {
+        printf("Maximum = %d\n", b);
+        printf("Minimum = %d\n", a);
+    } else {
+        printf("Both numbers are equal: %d\n", a);
+    }
+
+    return 0;
+}
